8c0ba00769beba14e64584f7f91eef19a47f37a8
Limon6912 <pepetazlimon@outlook.com>
2025-08-13 01:10:14 -0600

    se arreglaron errores esteticos

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -3,15 +3,15 @@ import { div } from "framer-motion/client"
 export const Contacto = () => {
     return (
         <div className="bg-gray-200 w-full p-10 flex gap-5  rounded-sm justify-center items-center overflow-hidden  ">
-            <div className="flex flex-col w-full sm:max-w-1/2 px-2 sm:px-10">
+            <div className="flex flex-col sm:sw-1/2 sm:max-w-1/2  px-2 sm:px-10">
                 <h2 className="text-3xl text-center w-full my-10 leading-[1.5]">Contáctanos para llenar tus eventos de  <samp className="text-[var(--color-amarillo)]"> alegría, <span className="text-[var(--color-verde)]">diversión</span> <samp className="text-[var(--color-azul)]">y energía.</samp>   </samp> </h2>
                 <form className=" flex flex-col w-full  mx-auto ">
                     <input type="email" name="" id="" className="bg-neutral-100 p-3 mb-2" placeholder="ejemplo@gmail.com" />
                     <textarea name="" id="" rows="8" className=" bg-neutral-100 p-3 resize-none"></textarea>
                 </form>
             </div>
-            <div className="hidden sm:block rounded-sm w-full overflow-hidden  max-h-[500px] ">
-                <div className=" grid grid-flow-col grid-rows-3 gap-2">
+            <div className="hidden sm:block  w-1/2">
+                <div className="rounded-sm w-full overflow-hidden  max-h-[500px] grid grid-flow-col grid-rows-3 gap-2">
                     <div className="row-span-3 rounded-sm overflow-hidden">
                         <video
                             autoPlay
