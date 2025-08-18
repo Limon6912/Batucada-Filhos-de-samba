5ff1ac65fe01b03e9a72d623599702f7bdf00f01
Limon6912 <pepetazlimon@outlook.com>
2025-08-18 15:55:59 -0600

    se optimizo la distribucionde los videos para dispositivos moviles

--- src/components/Galeria.jsx
+++ src/components/Galeria.jsx
@@ -5,35 +5,55 @@ export const Galeria = () => {
     return (
         <>
             {/* <div className='text-left w-full font-light text-2xl border-b-[1px] mb-2  border-[var(--color-azul)]'><samp className='text-[var(--color-azul)]'>Videos</samp></div> */}
-            <div className="grid grid-cols-3 lg:grid-cols-4 grid-rows-2 md:grid-rows-2 gap-2 overflow-hidden w-full max-h-[800px] aspect-square  m-auto ">
-                <div className="overflow-hidden sm:rounded-sm  rounded-r-sm  col-span-2">
+
+
+
+
+            <div className="grid grid-cols-3 grid-rows-9 gap-4 max-h-[850px]">
+
+                <div className="sm:rounded-sm overflow-hidden col-span-2 row-span-3 ">
                     <VideoGaleria src={videos.Corriendo} alt="Video de evento batucada filhos de samba" />
-
                 </div>
 
-                <div className="overflow-hidden sm:rounded-sm rounded-l-sm ">
+                <div className="sm:rounded-sm overflow-hidden row-span-4 col-start-3">
                     <VideoGaleria src={videos.sanRoman} alt="Video de evento batucada filhos de samba" />
-                    {/* <img src={imagenes.img3} alt="" className='w-full h-full object-cover aspect-square transition-transform duration-500 hover:scale-110 ' /> */}
+                </div>
 
+                <div className="sm:rounded-sm overflow-hidden  col-span-2 row-span-3 row-start-4">
+                    <VideoGaleria src={videos.caballito} alt="Video de evento batucada filhos de samba" />
                 </div>
-                <div className="overflow-hidden sm:rounded-sm row-span-2 hidden lg:block">
-                    <VideoGaleria src={videos.CantoF} alt="Video de evento batucada filhos de samba" />
-                </div>
-                <div className="overflow-hidden sm:rounded-sm   hidden lg:block">
-                    {/* <img
-                        src={imagenes.img9}
-                        alt="Foto grupal de la batucada filhos de samba"
-                        className="w-full h-full object-cover aspect-square transition-transform duration-500 hover:scale-110"
-                    /> */}
+
+                <div className="sm:rounded-sm overflow-hidden  col-span-2 row-span-3 col-start-1 row-start-7" >
                     <VideoGaleria src={videos.trompeta} alt="Video de evento batucada filhos de samba" />
                 </div>
-                <div className="overflow-hidden sm:rounded-sm  col-span-3 lg:col-span-2 ">
+
+                <div className="sm:rounded-sm overflow-hidden row-span-5 col-start-3 row-start-5">
+                    <VideoGaleria src={videos.CantoF} alt="Video de evento batucada filhos de samba" />
+                </div>
+
+            </div>
+
+            {/* <div className="grid grid-cols-3 grid-rows-6 gap-4 overflow-hidden w-full max-h-[800px]    ">
+                <div className="overflow-hidden sm:rounded-sm  rounded-r-sm col-span-2 row-span-2">
+                    <VideoGaleria src={videos.Corriendo} alt="Video de evento batucada filhos de samba" />
+                </div>
+                <div className="overflow-hidden sm:rounded-sm col-span-2 row-span-2 ">
+                    <VideoGaleria src={videos.CantoF} alt="Video de evento batucada filhos de samba" />
+                </div>
+                <div className="overflow-hidden sm:rounded-sm row-span-3 col-start-3">
+                    <VideoGaleria src={videos.sanRoman} alt="Video de evento batucada filhos de samba" />
+                </div>
+                <div className="overflow-hidden sm:rounded-sm  col-span-2 row-span-2 col-start-1 row-start-5">
+
+                    <VideoGaleria src={videos.trompeta} alt="Video de evento batucada filhos de samba" />
+                </div>
+                <div className="overflow-hidden sm:rounded-sm row-span-3 col-start-3 row-start-4">
                     <VideoGaleria src={videos.caballito} alt="Video de evento batucada filhos de samba" />
 
                 </div>
 
 
-            </div>
+            </div> */}
         </>
     )
 }
